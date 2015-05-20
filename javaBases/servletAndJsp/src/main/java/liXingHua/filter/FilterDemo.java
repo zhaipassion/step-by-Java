c6676ce4dbb61c085dc98034375faf83c9f7bf12
NorthFacing <0haizhu0@gmail.com>
2015-05-20 23:23:01 +0800

    filterDemo

---
+++ javaBases/servletAndJsp/src/main/java/liXingHua/filter/FilterDemo.java
@@ -0,0 +1,36 @@
+package liXingHua.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class FilterDemo implements Filter {
+    /**
+     * 程序启动的时候，此初始化方法就会执行
+     */
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        String initParam = filterConfig.getInitParameter("ref");
+        System.out.println("过滤器初始化，初始化参数：" + initParam);
+    }
+
+    /**
+     * 有访问服务器自愿的请求的时候会调用此过滤器
+     */
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
+        ServletException {
+        System.out.println("执行doFilter方法");
+    }
+
+    @Override
+    public void destroy() {
+       System.out.println("过滤器销毁。");
+    }
+
+}
---
+++ javaBases/servletAndJsp/src/main/java/liXingHua/servlet/ServletDemo.java
@@ -0,0 +1,6 @@
+package liXingHua.servlet;
+
+// 参考张孝祥范例就行了
+public class ServletDemo {
+
+}
