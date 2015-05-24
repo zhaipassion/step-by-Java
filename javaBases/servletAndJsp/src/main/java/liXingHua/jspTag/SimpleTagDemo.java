655f88c78f00adda0ace496f6e26adf303c2ec88
NorthFacing <0haizhu0@gmail.com>
2015-05-24 14:12:56 +0800

    jsp基本标签范例

---
+++ javaBases/servletAndJsp/src/main/java/liXingHua/jspTag/SimpleTagDemo.java
@@ -0,0 +1,23 @@
+package liXingHua.jspTag;
+
+import java.io.IOException;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.TagSupport;
+
+public class SimpleTagDemo extends TagSupport {
+
+    private static final long serialVersionUID = -8856428367894979108L;
+
+    public int doStartTag() throws JspException {
+        JspWriter out = super.pageContext.getOut();
+        try {
+            out.println("<h1>Hello world! <h1>");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return TagSupport.SKIP_BODY;
+    }
+
+}
