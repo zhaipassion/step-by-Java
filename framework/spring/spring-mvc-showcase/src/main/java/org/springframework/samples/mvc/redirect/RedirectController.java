33dd5f8096f1dee50b3c1ed7997631210bc64d83
NorthFacing <0haizhu0@gmail.com>
2015-06-04 15:37:43 +0800

    重定向学习

--- framework/spring/spring-mvc-showcase/src/main/java/org/springframework/samples/mvc/redirect/RedirectController.java
+++ framework/spring/spring-mvc-showcase/src/main/java/org/springframework/samples/mvc/redirect/RedirectController.java
@@ -16,32 +16,33 @@ import org.springframework.web.util.UriComponentsBuilder;
 @Controller
 @RequestMapping("/redirect")
 public class RedirectController {
-	
-	private final ConversionService conversionService;
 
-	@Inject
-	public RedirectController(ConversionService conversionService) {
-		this.conversionService = conversionService;
-	}
+    private final ConversionService conversionService;
 
-	@RequestMapping(value="/uriTemplate", method=RequestMethod.GET)
-	public String uriTemplate(RedirectAttributes redirectAttrs) {
-		redirectAttrs.addAttribute("account", "a123");  // Used as URI template variable
-		redirectAttrs.addAttribute("date", new LocalDate(2011, 12, 31));  // Appended as a query parameter
-		return "redirect:/redirect/{account}";
-	}
+    @Inject
+    public RedirectController(ConversionService conversionService) {
+        this.conversionService = conversionService;
+    }
 
-	@RequestMapping(value="/uriComponentsBuilder", method=RequestMethod.GET)
-	public String uriComponentsBuilder() {
-		String date = this.conversionService.convert(new LocalDate(2011, 12, 31), String.class);
-		UriComponents redirectUri = UriComponentsBuilder.fromPath("/redirect/{account}").queryParam("date", date)
-				.build().expand("a123").encode();
-		return "redirect:" + redirectUri.toUriString();
-	}
+    @RequestMapping(value = "/uriTemplate", method = RequestMethod.GET)
+    public String uriTemplate(RedirectAttributes redirectAttrs) {
+        redirectAttrs.addAttribute("account", "a123");  // Used as URI template variable
+        redirectAttrs.addAttribute("date", new LocalDate(2011, 12, 31));  // Appended as a query
+                                                                         // parameter
+        return "redirect:/redirect/{account}";
+    }
 
-	@RequestMapping(value="/{account}", method=RequestMethod.GET)
-	public String show(@PathVariable String account, @RequestParam(required=false) LocalDate date) {
-		return "redirect/redirectResults";
-	}
+    @RequestMapping(value = "/uriComponentsBuilder", method = RequestMethod.GET)
+    public String uriComponentsBuilder() {
+        String date = this.conversionService.convert(new LocalDate(2011, 12, 31), String.class);
+        UriComponents redirectUri = UriComponentsBuilder.fromPath("/redirect/{account}").queryParam("date", date)
+            .build().expand("a123").encode();
+        return "redirect:" + redirectUri.toUriString();
+    }
+
+    @RequestMapping(value = "/{account}", method = RequestMethod.GET)
+    public String show(@PathVariable String account, @RequestParam(required = false) LocalDate date) {
+        return "redirect/redirectResults";
+    }
 
 }
