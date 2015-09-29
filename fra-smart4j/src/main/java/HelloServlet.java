a79484d3a29833ba870cf479d9c1f94aec47eb6c
Bob <0haizhu0@gmail.com>
2015-09-29 21:26:26 +0800

    servlet demo

---
+++ fra-smart4j/src/main/java/HelloServlet.java
@@ -0,0 +1,23 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by Bob on 2015/9/29.
+ */
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String currentTime = dateFormat.format(new Date());
+        request.setAttribute("currentTime", currentTime);
+        request.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(request, response);
+    }
+}
