76280551b6f20fb6ef3a4bc25049dcd487d90f2d
Bob <0haizhu0@gmail.com>
2015-11-03 15:14:07 +0800

    list subList

---
+++ java-base/src/main/java/com/bob/collection/c02_List/Demo.java
@@ -0,0 +1,31 @@
+package com.bob.collection.c02_List;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Bob on 2015/11/3.
+ */
+public class Demo {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 39; i++) {
+            list.add(i + 1);
+        }
+
+        List<Integer> subList;
+        Integer perCount = 10;// 每次处理的个数
+        while (true) {
+            if (list.size() > perCount) {
+                subList = list.subList(0, perCount);
+                list = list.subList(perCount, list.size());
+                System.out.println(subList);
+            } else {
+                System.out.println(list);
+                break;
+            }
+        }
+
+    }
+
+}
